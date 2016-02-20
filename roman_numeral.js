2250e571b595edd38ebf0da52405def5c9bdbb85
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2016-02-19 22:53:16 -0500

    design improvements

--- roman_numeral.js
+++ roman_numeral.js
@@ -1,55 +1,58 @@
 /* Convert the given number into a roman numeral.*/
 
+var romanSpec = {
+  1000: 'M',
+  900: 'CM',
+  500: 'D',
+  400: 'CD',
+  100: 'C',
+  90: 'XC',
+  50: 'L',
+  40: 'XL',
+  10: 'X',
+  9: 'IX',
+  5: 'V',
+  4: 'IV',
+  1: 'I'
+};
+var romanSpecKeys = Object.keys(romanSpec).sort(function (a,b) {
+  return b-a;
+});
+
 /**
  * @param {int} num
  * @return {array}
- * depending on the spec, returns an array with num details
+ * returns an array with num decomposition
  */
-function getNumDetails(num){
-  var n = num;
+function getNumDecomposition(num){
   var arr = [];
-  var spec = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  for(var i = 0; i < spec.length; i++){
-    arr.push(parseInt(n / spec[i]));
-    n = n % spec[i];
+  for(var i = 0; i < romanSpecKeys.length; i++){
+    arr.push(Math.floor(num / romanSpecKeys[i]));
+    num = num % romanSpecKeys[i];
   }
   return arr;
 }
 
 /**
- * @param {string, int} romanLetter,numberOfTimes
- * @return {string}
- * returns a string containing `romanLetter` concatenated a `numberOfTimes`
- */
-function concatRoman(romanLetter, numberOfTimes){
-  var result = '';
-  for(var i = 1; i <= numberOfTimes; i++){
-    result += romanLetter;
-  }
-  return result;
-}
-
-/**
  * @param {array} numDetailed
  * @return {string}
- * returns a roman number based on `romanSpec` and `spec` from @getNumDetails
+ * returns a roman number based on numDecomposition
  */
-function buildRoman(numDetailed){
-  var romanSpec = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+function buildRoman(numDecomposed){
   var result = '';
-  for(var i = 0; i < numDetailed.length; i++){
-    result += concatRoman(romanSpec[i], numDetailed[i]);
+  for(var i = 0; i < romanSpecKeys.length; i++){
+    result += romanSpec[romanSpecKeys[i]].repeat(numDecomposed[i]);
   }
   return result;
 }
 
 
-function convert(num){
-  
-  var num_details = getNumDetails(num);
-  var num_roman = buildRoman(num_details);
-  
-  return num_roman;
+function intoRoman(num){
+
+  var numDecomposed = getNumDecomposition(num);
+  var numRoman = buildRoman(numDecomposition);
+
+  return numRoman;
 }
 
 convert(1099);
--- where_art_though.js
+++ where_art_though.js
@@ -1,21 +1,19 @@
-/* 
-Make a function that looks through an array of objects (first argument) and returns an array of all objects that have 
+/*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have
 matching property and value pairs (second argument). Each property and value pair of the source object has to be present
 in the object from the collection if it is to be included in the returned array.
 
-For example, if the first argument is 
-[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], 
-and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), 
+For example, if the first argument is
+[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument),
 because it contains the property and it's value, that was passed on as the second argument.
 */
 
 function where(collection, source) {
-  
-  var arr = [];
-  var sourceKeys = Object.keys(source);
-  // delcaration out of for loop to save memory
-  var check;
-  
+  var arr = [],
+      sourceKeys = Object.keys(source),
+      check = true;
+
   // for each object in collection
   for(var i = 0; i < collection.length ; i++){
     // for each key in source object
@@ -33,7 +31,7 @@ function where(collection, source) {
       arr.push(collection[i]);
     }
   }
-  
+
   return arr;
 }
 
