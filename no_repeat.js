2f4a043a6c1a7b517327bde5326dc70a73efc489
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2016-03-09 18:16:22 -0500

    add another solution to no_repeat.js

--- no_repeat.js
+++ no_repeat.js
@@ -7,7 +7,7 @@ For example, aab should return 2 because it has 6 total permutations (aab, aab, 
  * return: [Array]
  * Tales a string as input and returns a list of strings containing all the possible permutations of `s`
  */
-function permutations(s) {
+function permutations_1(s) {
 
     var res = []
     var perms = [''];
@@ -36,4 +36,40 @@ function permutations(s) {
     return allPermutations;
 }
 
-console.log(permutations('abcd'));
+console.log(permutations_1('abcd'));
+
+// other solution
+
+function nPlus1 (s, sPerms, c) {
+
+    var nPlus1Perms = [],
+        perm;
+
+    for (var i = 0; i <= s.length; i++) {
+
+        for (var j = 0; j < sPerms.length; j++) {
+
+            perm = sPerms[j].slice(0,i) + c + sPerms[j].slice(i);
+            nPlus1Perms.push(perm);
+        }
+    }
+
+    return nPlus1Perms;
+}
+
+function permutations (s) {
+
+    if (s.length === 1 || s.length === 0) {
+        return [s];
+    }
+
+    if (s.length > 2) {
+        var perms = permutations(s.slice(0,s.length - 1));
+    } else {
+        return nPlus1(s[0], [s[0]], s[1]);
+    }
+
+    return nPlus1(s.slice(0, s.length - 1), perms, s[s.length - 1]);
+}
+
+console.log(permutations('abc'));
