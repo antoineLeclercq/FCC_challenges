8cf3e1276edffbc2669ed569eff17d9bfd44b36d
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2016-03-09 20:56:30 -0500

    solution to no_repeat challenge

--- no_repeat.js
+++ no_repeat.js
@@ -2,34 +2,57 @@
 
 For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating. */
 
-/**
- * params: [string], [s]
- * return: [Array]
- * Tales a string as input and returns a list of strings containing all the possible permutations of `s`
- */
-
-function permutations (s) {
+function permAlone(s) {
     
-    var allPermutations = [],
+    var sLength = s.length;
+    
+    var permutations = function (s) {
+    
+        var allPermutations = [],
         perms = [''],
         perm;
-    
-    // if string is more than 1 char, recursively call permutations until s is only one char
-    if (s.length > 1) {
-        perms = permutations_2(s.slice(0, s.length - 1));
-    }
-    
-    // having `perms` equal to the permutations of the `s` associated with the previous function call
-    // for each position in `s`, loop through all permutations
-    // and add the additional char in `s` that was not in the previous function call to each permutation for each position in s
-    for (var i = 0; i < s.length; i++) {
-        
-        for (var j = 0; j < perms.length; j++) {
-            
-            perm = perms[j].slice(0,i) + s[s.length - 1] + perms[j].slice(i);
-            allPermutations.push(perm);
+
+        // if string is more than 1 char, recursively call permutations until s is only one char
+        if (s.length > 1) {
+            perms = permutations(s.slice(0, s.length - 1));
         }
-    }
-    
-    return allPermutations;
+
+        // having `perms` equal to the permutations of the `s` associated with the previous function call
+        // for each position in `s`, loop through all permutations
+        // and add the additional char in `s` that was not in the previous function call
+        // to each permutation for each position in s
+        for (var i = 0; i < s.length; i++) {
+
+            for (var j = 0; j < perms.length; j++) {
+
+                perm = perms[j].slice(0,i) + s[s.length - 1] + perms[j].slice(i);
+                
+                // if `perm` is the same length as initial string passed in @permAlone
+                // check if `perm` has 2 repeated chars, if not push perm to `allPermutations
+                if (perm.length === sLength) {
+
+                    var checker = true;
+
+                    for (var k = 0; k < perm.length; k++) {
+
+                        if (perm[k] === perm[k + 1]) {
+                            checker = false;
+                            break;
+                        }
+                    }
+
+                    if (checker) {
+                        allPermutations.push(perm);
+                    }
+                } 
+                else {
+                    allPermutations.push(perm);
+                }
+            }
+        }
+
+        return allPermutations;
+    };
+
+    return permutations(s).length;
 }
